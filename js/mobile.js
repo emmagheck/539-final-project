30d16e798dfb024c84f42b35d1654015e2124703
Emma <124534664+emmagheck@users.noreply.github.com>
2023-12-06 22:14:21 -0500

    Add files via upload

--- js/mobile.js
+++ js/mobile.js
@@ -1,8 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const flipBoxes = document.querySelectorAll(".flip-box");
+// document.addEventListener("DOMContentLoaded", function() {
+//     const flipBoxes = document.querySelectorAll(".flip-box");
 
-    flipBoxes.forEach(function(box) {
-      box.addEventListener("click", function() {
-        this.querySelector(".flip-box-inner").style.transform = "rotateY(180deg)";});
+//     flipBoxes.forEach(function(box) {
+//       box.addEventListener("click", function() {
+//         this.querySelector(".flip-box-inner").style.transform = "rotateY(180deg)";});
+//     });
+//   });
+
+document.addEventListener("DOMContentLoaded", function () {
+    var flipBoxes = document.querySelectorAll(".flip-box");
+
+    flipBoxes.forEach(function (flipBox) {
+      flipBox.addEventListener("click", function () {
+
+        flipBox.classList.toggle("flipped");
+      });
     });
   });
